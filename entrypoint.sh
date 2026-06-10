626650ca8238275ca2f48fdf13fd03160c6c8d93
jojo boy <josephedward07017@gmail.com>
2026-06-10 17:17:15 +0100

    Final updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,20 @@ python manage.py collectstatic --noinput
 echo ">>> Running migrations..."
 python manage.py migrate
 
+echo ">>> Creating superuser if not exists..."
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser(
+        '$DJANGO_SUPERUSER_USERNAME',
+        '$DJANGO_SUPERUSER_EMAIL',
+        '$DJANGO_SUPERUSER_PASSWORD'
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
+
 echo ">>> Starting services..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
